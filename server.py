ea52bb4fdca9145e05b579943391f94b17958f83
Jeff Landers <jeff.landers12@gmail.com>
2020-04-24 16:38:03 +0200

    uploading project

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<page>')
+def page_name(page):
+    return render_template(f'{page}')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong, try again'
